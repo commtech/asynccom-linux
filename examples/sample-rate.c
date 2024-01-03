ac37244a1835dc13765298dc7da348c3dd907a0e
jdroney <josh@fastcomproducts.com>
2024-01-03 11:08:27 -0600

    Fixed the sample-rate example.

--- examples/sample-rate.c
+++ examples/sample-rate.c
@@ -7,14 +7,11 @@
 int main(int argc, char **argv)
 {
     int fd = 0;
-    unsigned rate;
     int samplerate;
 
     fd = open("/dev/ttyUSB0", O_RDWR);
     
-    samplerate = atoi(argv[1]);
-
-    ioctl(fd, IOCTL_ASYNCCOM_GET_SAMPLE_RATE, &rate);
+    ioctl(fd, IOCTL_ASYNCCOM_GET_SAMPLE_RATE, &samplerate);
 
     ioctl(fd, IOCTL_ASYNCCOM_SET_SAMPLE_RATE, samplerate);
 
